e7561b1531bc6d67f37937a499a29e021892a460
enter-a-usernamee <mjames29@live.spcollege.edu>
2023-08-31 14:36:39 -0400

    Initial Commit

---
+++ Checklists/Data Models/ChecklistItem.swift
@@ -0,0 +1,85 @@
+/// Copyright (c) 2020 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import Foundation
+import UserNotifications
+
+class ChecklistItem: NSObject, Codable {
+  var text = ""
+  var checked = false
+  var dueDate = Date()
+  var shouldRemind = false
+  var itemID = -1
+  
+  override init() {
+    super.init()
+    itemID = DataModel.nextChecklistItemID()
+  }
+  
+  deinit {
+    removeNotification()
+  }
+  
+  func scheduleNotification() {
+    removeNotification()
+    if shouldRemind && dueDate > Date() {
+      // 1
+      let content = UNMutableNotificationContent()
+      content.title = "Reminder:"
+      content.body = text
+      content.sound = UNNotificationSound.default
+      // 2
+      let calendar = Calendar(identifier: .gregorian)
+      let components = calendar.dateComponents(
+        [.year, .month, .day, .hour, .minute],
+        from: dueDate)
+      // 3
+      let trigger = UNCalendarNotificationTrigger(
+        dateMatching: components,
+        repeats: false)
+      // 4
+      let request = UNNotificationRequest(
+        identifier: "\(itemID)",
+        content: content,
+        trigger: trigger)
+      // 5
+      let center = UNUserNotificationCenter.current()
+      center.add(request)
+      print("Scheduled: \(request) for itemID: \(itemID)")
+    }
+  }
+  
+  func removeNotification() {
+    let center = UNUserNotificationCenter.current()
+    center.removePendingNotificationRequests(withIdentifiers: ["\(itemID)"])
+  }
+}
